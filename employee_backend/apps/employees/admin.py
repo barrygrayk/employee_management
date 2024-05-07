abd057c39d812f8152ff15b01c146b47f7845d5d
Barry Kapelembe <barry.kapelembeg@gmail.com>
2024-05-07 19:15:12 +0200

    Add django admin for skills and employee models

--- employee_backend/apps/employees/admin.py
+++ employee_backend/apps/employees/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from apps.employees.models import Employee, Skill
 
-# Register your models here.
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email_address")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("skills",)
+
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ("name", "years_of_experience", "seniority")
+    search_fields = ("name",)
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Skill, SkillAdmin)
